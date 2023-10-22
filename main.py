86643eaea28b294d525a600419d1abd444c9ebd6
VikramKindo <142928783+VikramKindo@users.noreply.github.com>
2023-10-21 18:27:37 -0700

    Create main.py

---
+++ main.py
@@ -0,0 +1,67 @@
+# Required Libraries
+import streamlit as st
+# Assuming you have a function 'get_response' that gets the chatbot's response
+# def get_response(user_input):
+#     return "This is the chatbot's reply to " + user_input
+
+# Streamlit UI
+
+# App Title
+st.title("ChatGPT-like Interface")
+
+# Sidebar for user instructions or additional information
+st.sidebar.header("Instructions")
+st.sidebar.text("Enter your message and click 'Send'.\nProvide feedback using thumbs up/down.")
+
+# Layout
+col1, col2 = st.beta_columns([3,1])
+
+# Chat history
+history = []
+
+# Check if there's any previous interaction stored in the session state
+if 'history' not in st.session_state:
+    st.session_state.history = []
+else:
+    history = st.session_state.history
+
+# Display chat history
+for chat in history:
+    if chat['type'] == 'user':
+        col1.write(f"You: {chat['message']}", unsafe_allow_html=True)
+    else:
+        col1.write(f"ChatGPT: {chat['message']}", unsafe_allow_html=True)
+
+# User input
+with col1:
+    user_input = st.text_input("You")
+
+# Send button
+if col2.button('Send'):
+    # Add the user's message to the chat history
+    history.append({'type': 'user', 'message': user_input})
+    
+    # Get chatbot's response (in this example, a dummy function)
+    response = get_response(user_input)
+    history.append({'type': 'bot', 'message': response})
+
+    # Update the session state with the updated chat history
+    st.session_state.history = history
+
+    # Reload the page to show the updated chat history
+    st.experimental_rerun()
+
+# Feedback
+feedback = st.radio("Was this helpful?", ["👍", "👎"])
+
+# Comments
+comments = st.text_area("Any comments?")
+
+# Submit feedback button
+if st.button('Submit Feedback'):
+    # Here you could send the feedback and comments to a database or any other storage.
+    st.success("Thanks for your feedback!")
+
+# Dummy function for chatbot's response (remove this if you have a real function)
+def get_response(user_input):
+    return "This is the chatbot's reply to " + user_input
