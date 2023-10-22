fad0ad52a5fd96f4d2eb242fabd6d4c7e5b4c95a
VikramKindo <142928783+VikramKindo@users.noreply.github.com>
2023-10-21 22:57:45 -0700

    Update main.py

--- main.py
+++ main.py
@@ -1,36 +1,60 @@
-# Required Libraries
 import streamlit as st
-# Assuming you have a function 'get_response' that gets the chatbot's response
+import csv
+import os
+
+# Filepaths to store data
+HISTORY_FILE_PATH = "chat_history.csv"
+FEEDBACK_FILE_PATH = "feedback.csv"
+
+# Function to load existing chat data
+def load_history():
+    if os.path.exists(HISTORY_FILE_PATH):
+        with open(HISTORY_FILE_PATH, 'r') as file:
+            reader = csv.reader(file)
+            return [row for row in reader]
+    return []
+
+# Function to load feedback
+def load_feedback():
+    if os.path.exists(FEEDBACK_FILE_PATH):
+        with open(FEEDBACK_FILE_PATH, 'r') as file:
+            reader = csv.reader(file)
+            return [row for row in reader]
+    return []
+
+# Function to save chat data
+def save_history(history):
+    with open(HISTORY_FILE_PATH, 'w') as file:
+        writer = csv.writer(file)
+        writer.writerows(history)
+
+# Function to save feedback
+def save_feedback(feedback):
+    with open(FEEDBACK_FILE_PATH, 'w') as file:
+        writer = csv.writer(file)
+        writer.writerows(feedback)
+
+# Dummy function for chatbot's response (remove this if you have a real function)
 def get_response(user_input):
     return "This is the chatbot's reply to " + user_input
 
+history = load_history()
+feedback_data = load_feedback()
+
 # Streamlit UI
 
-# App Title
 st.title("ChatGPT-like Interface")
-
-# Sidebar for user instructions or additional information
 st.sidebar.header("Instructions")
 st.sidebar.text("Enter your message and click 'Send'.\nProvide feedback using thumbs up/down.")
 
-# Layout
 col1, col2 = st.columns([3,1])
-
-# Chat history
-history = []
-
-# Check if there's any previous interaction stored in the session state
-if 'history' not in st.session_state:
-    st.session_state.history = []
-else:
-    history = st.session_state.history
 
 # Display chat history
 for chat in history:
-    if chat['type'] == 'user':
-        col1.write(f"You: {chat['message']}", unsafe_allow_html=True)
+    if chat[0] == 'user':
+        col1.write(f"You: {chat[1]}", unsafe_allow_html=True)
     else:
-        col1.write(f"ChatGPT: {chat['message']}", unsafe_allow_html=True)
+        col1.write(f"ChatGPT: {chat[1]}", unsafe_allow_html=True)
 
 # User input
 with col1:
@@ -38,30 +62,18 @@ with col1:
 
 # Send button
 if col2.button('Send'):
-    # Add the user's message to the chat history
-    history.append({'type': 'user', 'message': user_input})
-    
-    # Get chatbot's response (in this example, a dummy function)
+    history.append(['user', user_input])
     response = get_response(user_input)
-    history.append({'type': 'bot', 'message': response})
-
-    # Update the session state with the updated chat history
-    st.session_state.history = history
-
-    # Reload the page to show the updated chat history
+    history.append(['bot', response])
+    save_history(history)
     st.experimental_rerun()
 
 # Feedback
 feedback = st.radio("Was this helpful?", ["👍", "👎"])
-
-# Comments
 comments = st.text_area("Any comments?")
 
 # Submit feedback button
 if st.button('Submit Feedback'):
-    # Here you could send the feedback and comments to a database or any other storage.
+    feedback_data.append([feedback, comments])
+    save_feedback(feedback_data)
     st.success("Thanks for your feedback!")
-
-# Dummy function for chatbot's response (remove this if you have a real function)
-def get_response(user_input):
-    return "This is the chatbot's reply to " + user_input
